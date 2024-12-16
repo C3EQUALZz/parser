6eee6fb110ab43d043b9886152f19ecb6ef4fa4f
c3equalz <dan.kovalev2013@gmail.com>
2024-12-16 11:03:57 +0300

    changed indents

--- app/infrastructure/services/product_marketplace/wildberries.py
+++ app/infrastructure/services/product_marketplace/wildberries.py
@@ -5,14 +5,12 @@ class WildberriesProductService:
     """
     Service layer core according to DDD, which using a unit of work, will perform operations on the domain model.
     """
+
     def __init__(self, parser: AbstractWildBerriesParser) -> None:
         self._parser = parser
 
     async def get_all_products_with_name(self):
         ...
 
-
-
     async def get_all_categories(self):
         ...
-
